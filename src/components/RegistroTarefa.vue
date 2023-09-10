bf7f13a46d54be1f8281855a64bb4c3c650b7557
andreinafuku <55460421+andreinafuku@users.noreply.github.com>
2023-09-10 07:10:38 -0300

    Criação

---
+++ src/components/RegistroTarefa.vue
@@ -0,0 +1,31 @@
+<template>
+    <BoxTarefa>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa.descricao|| "Tarefa sem descrição" }}
+            </div>
+            <div class="column">
+                <VisorCronometro :tempo-em-segundos="tarefa.duracaoEmSegundos" />
+            </div>
+        </div>
+    </BoxTarefa>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import VisorCronometro from './VisorCronometro.vue';
+import ITarefa from '@/interfaces/ITarefa';
+import BoxTarefa from './BoxTarefa.vue';
+
+export default defineComponent({
+    name: "RegistroTarefa",
+    components: { VisorCronometro, BoxTarefa },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+            required: true
+        }
+    }
+});
+</script>
+
